3276b866bbb0d918f567f39fa3d7c7ca5215d02d
prakhar308 <khandelwalprakhar30@gmail.com>
2020-03-30 15:54:51 +0530

    add id field in recipes

--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -9,24 +9,28 @@ class RecipeApp extends React.Component{
 		this.state = {
 			recipes : [
 				{
+					id : 1,
 					img : "pasta.jpg",
 					title : "Pasta",
 					ingredients : ['macroni', 'spices', 'water'],
 					instructions : "Mix ingredients"
 				},
 				{
+					id : 2,
 					img : "cold-coffee.jpg",
 					title : "Cold Coffee",
 					ingredients : [ '2 cup milk','2 1/2 teaspoon coffee','1 cup vanilla ice-cream'],
 					instructions : "Mix ingredients"
 				},
 				{
+					id : 3,
 					img : "egg-bhurji.jpg",
 					title : "Egg Bhurji",
 					ingredients : ['2-3 eggs','2 tablespoon oil','onoin and green chilli finely chopped'],
 					instructions : "Mix ingredients"	
 				}
-			] 
+			],
+			nextRecipeId : 4 
 		}
 	}
 	render(){
--- src/RecipeList.js
+++ src/RecipeList.js
@@ -4,8 +4,8 @@ import Recipe from './Recipe'
 
 class RecipeList extends React.Component{
 	render(){
-		const recipes = this.props.recipes.map((r,index) => (
-			<Recipe key={index} {...r}/>
+		const recipes = this.props.recipes.map((r) => (
+			<Recipe key={r.id} {...r}/>
 		))
 		return(
 			<div className="recipe-list">
