aa970a0b551e8225bb185440529670da8a733612
prakhar308 <khandelwalprakhar30@gmail.com>
2020-03-30 15:49:00 +0530

    add state inside ReactApp component and remove defaultProps from RecipeList we add state inside ReactApp because it is the parent of other components and it passes state as props to it's children

--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -4,14 +4,39 @@ import RecipeList from './RecipeList'
 import Navbar from './Navbar'
 
 class RecipeApp extends React.Component{
-  render(){
-    return(
-      <div className="App">
-      	<Navbar />
-      	<RecipeList />
-      </div>  
-    );
-  }
+	constructor(props){
+		super(props);
+		this.state = {
+			recipes : [
+				{
+					img : "pasta.jpg",
+					title : "Pasta",
+					ingredients : ['macroni', 'spices', 'water'],
+					instructions : "Mix ingredients"
+				},
+				{
+					img : "cold-coffee.jpg",
+					title : "Cold Coffee",
+					ingredients : [ '2 cup milk','2 1/2 teaspoon coffee','1 cup vanilla ice-cream'],
+					instructions : "Mix ingredients"
+				},
+				{
+					img : "egg-bhurji.jpg",
+					title : "Egg Bhurji",
+					ingredients : ['2-3 eggs','2 tablespoon oil','onoin and green chilli finely chopped'],
+					instructions : "Mix ingredients"	
+				}
+			] 
+		}
+	}
+	render(){
+	 return(
+	   <div className="App">
+	   	<Navbar />
+	   	<RecipeList recipes={this.state.recipes}/>
+	   </div>  
+	 );
+	}
 }
 
 export default RecipeApp;
--- src/RecipeList.js
+++ src/RecipeList.js
@@ -3,30 +3,6 @@ import './RecipeList.css'
 import Recipe from './Recipe'
 
 class RecipeList extends React.Component{
-
-	static defaultProps = {
-		recipes : [
-			{
-				img : "pasta.jpg",
-				title : "Pasta",
-				ingredients : ['macroni', 'spices', 'water'],
-				instructions : "Mix ingredients"
-			},
-			{
-				img : "cold-coffee.jpg",
-				title : "Cold Coffee",
-				ingredients : [ '2 cup milk','2 1/2 teaspoon coffee','1 cup vanilla ice-cream'],
-				instructions : "Mix ingredients"
-			},
-			{
-				img : "egg-bhurji.jpg",
-				title : "Egg Bhurji",
-				ingredients : ['2-3 eggs','2 tablespoon oil','onoin and green chilli finely chopped'],
-				instructions : "Mix ingredients"	
-			}
-		]
-	}
-
 	render(){
 		const recipes = this.props.recipes.map((r,index) => (
 			<Recipe key={index} {...r}/>
